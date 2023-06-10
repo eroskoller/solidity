700199e5fa7683605011dc275e3e1f09585457a2
eroskoller <eroskoller@gmail.com>
2023-06-10 10:17:58 -0300

    Update VendaBonus.sol

--- VendaBonus.sol
+++ VendaBonus.sol
@@ -8,20 +8,12 @@ contract VendaBonus {
     string public nomeVendedor;
     uint256 public bonus;
 
-function setBonus(uint256 _bonus)public {
-    bonus = _bonus;
-}
 
-function setNomeVendedor(string memory _nomeVendedor) public {
-    nomeVendedor = _nomeVendedor;
-}
+    constructor(uint256 _bonus , string memory _nomeVendedor){
+        bonus = _bonus;
+        nomeVendedor = _nomeVendedor;
 
-
-    // constructor(uint256 _bonus , string memory _nomeVendedor){
-    //     bonus = _bonus;
-    //     nomeVendedor = _nomeVendedor;
-
-    // }
+    }
 
 
     function calculaBonus(uint256 _venda) public view  returns (uint256){
