5fb7f710ebf7a3a938016208e183060b27cfeb18
eroskoller <eroskoller@gmail.com>
2023-06-10 09:43:50 -0300

    Update VendaBonus.sol

--- VendaBonus.sol
+++ VendaBonus.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-//0xf8e81D47203A594245E36C48e151709F0C19fBe8
+//0xDA0bab807633f07f013f94DD0E6A4F96F8742B53
 
 pragma solidity ^0.8.15;
 
@@ -12,19 +12,19 @@ function setBonus(uint256 _bonus)public {
     bonus = _bonus;
 }
 
-
 function setNomeVendedor(string memory _nome) public {
     nomeVendedor = _nome;
 }
 
+
     // constructor(uint256 _bonus , string memory _nomeVendedor){
     //     bonus = _bonus;
     //     nomeVendedor = _nomeVendedor;
+
     // }
 
 
     function calculaBonus(uint256 _venda) public view  returns (uint256){
-        return _venda * bonus;
+        return  (_venda * bonus) / 100 ;
     }
-
 }
