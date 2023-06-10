ad30bf59a1979299275fb2e3e51e5c98f7c94d25
eroskoller <eroskoller@gmail.com>
2023-06-10 09:38:29 -0300

    Update VendaBonus.sol

--- VendaBonus.sol
+++ VendaBonus.sol
@@ -4,23 +4,12 @@ pragma solidity ^0.8.15;
 
 contract VendaBonus {
 
-
-
-
     string public nomeVendedor;
-
     uint256 bonus;
 
-
-
-
 function setBonus(uint256 _bonus)public {
-
     bonus = _bonus;
-
 }
-
-
 
 
 function setNomeVendedor(string memory _nome) public {
@@ -29,24 +18,17 @@ function setNomeVendedor(string memory _nome) public {
 
 }
 
-
-
     // constructor(uint256 _bonus , string memory _nomeVendedor){
 
     //     bonus = _bonus;
 
     //     nomeVendedor = _nomeVendedor;
 
-
-
-
     // }
 
 
     function calculaBonus(uint256 _venda) public view  returns (uint256){
-
         return _venda * bonus;
-
     }
 
 }
