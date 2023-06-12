238e329fd0a6fe9e38e41fae81a5b104a6360174
eroskoller <eroskoller@gmail.com>
2023-06-12 17:55:09 -0300

    Update 03_aluguel_homework.sol

--- 03_aluguel_homework.sol
+++ 03_aluguel_homework.sol
@@ -6,82 +6,120 @@ pragma solidity ^0.8.15;
 //required revert Struct modifier
 
 contract AlugueHomeWork {
-
-
- struct Aluguel {
-        uint codigo;
+    struct Aluguel {
+        uint256 codigo;
         string locador;
         string locatario;
-        uint[36] parcelas;
+        uint256[36] parcelas;
     }
 
-   mapping(uint => Aluguel) public alugueis;
+    mapping(uint256 => Aluguel) public alugueis;
 
-  
-     modifier validaValorParcela (uint _valor) {
+    modifier validaExistenciaContrato(uint256 _codigo) {
+        Aluguel memory a = alugueis[_codigo];
+        require(a.codigo > 0, "Contrato inexistente!");
+        _;
+    }
+
+    modifier validaValorParcela(uint256 _valor) {
         require(_valor >= 0, "Valor invalido!");
         _;
     }
 
-    modifier validaCodigoContrato (uint _codigo) {
+    modifier validaCodigoContrato(uint256 _codigo) {
         require(_codigo >= 0, "Codigo invalido!");
         _;
     }
 
-    modifier validaMes (uint _mes) {
+    modifier validaMes(uint256 _mes) {
         require(_mes < 36, "Mes invalido!");
         _;
     }
 
-
-    function montaParcelas(uint _valor) private pure returns (uint[36] memory parcelas) {
-        for (uint i = 0; i < 36; i++) {
+    function montaParcelas(uint256 _valor)
+        private
+        pure
+        returns (uint256[36] memory parcelas)
+    {
+        for (uint256 i = 0; i < 36; i++) {
             parcelas[i] = _valor;
         }
         return parcelas;
     }
 
-     function contratarAluguel(uint _codigo, string memory _locador, string memory _locatario, uint _parcela) 
-     external  validaValorParcela(_parcela)
-     validaCodigoContrato(_codigo) returns (bool) {
-        Aluguel memory _aluguel = Aluguel(_codigo, _locador, _locatario, montaParcelas(_parcela));
+    function contratarAluguel(
+        uint256 _codigo,
+        string memory _locador,
+        string memory _locatario,
+        uint256 _parcela
+    )
+        external
+        validaValorParcela(_parcela)
+        validaCodigoContrato(_codigo)
+        returns (bool)
+    {
+        Aluguel memory _aluguel = Aluguel(
+            _codigo,
+            _locador,
+            _locatario,
+            montaParcelas(_parcela)
+        );
         alugueis[_codigo] = _aluguel;
         return true;
     }
 
-
-    function valorPorMes(uint _codigo, uint mes) public view
-    validaCodigoContrato(_codigo) validaMes(mes)
-     returns( uint256){
+    function valorPorMes(uint256 _codigo, uint256 mes)
+        public
+        view
+        validaExistenciaContrato(_codigo)
+        validaCodigoContrato(_codigo)
+        validaMes(mes)
+        returns (uint256)
+    {
         return alugueis[_codigo].parcelas[mes];
     }
 
-    function nomesDasPartes(uint codigo) public view returns (string memory, string memory){
-        Aluguel memory a = alugueis[codigo];
+    function nomesDasPartes(uint256 _codigo)
+        public
+        view
+        validaExistenciaContrato(_codigo)
+        returns (string memory, string memory)
+    {
+        Aluguel memory a = alugueis[_codigo];
         return (a.locador, a.locatario);
     }
 
-    function alteraNomeContratantes(uint _codigo, uint option, string memory name) public  view
-    validaCodigoContrato(_codigo)
-     returns (bool) {
+    function alteraNomeContratantes(
+        uint256 _codigo,
+        uint256 option,
+        string memory name
+    ) public view
+    validaExistenciaContrato(_codigo) 
+    validaCodigoContrato(_codigo) returns (bool) {
         Aluguel memory a = alugueis[_codigo];
-        if(option == 1){
+        if (option == 1) {
             a.locador = name;
-        }else if(option == 2){
+        } else if (option == 2) {
             a.locatario = name;
-        }else{
+        } else {
             revert("Opcao invalida!");
         }
         return true;
     }
 
-    function alteraValorMesesRestantes(uint codigo,uint startMes, uint256 acrecimo) public view returns (bool){
-        Aluguel memory a = alugueis[codigo];
-        for(uint i = startMes ; i < a.parcelas.length; i ++ ){
-            a.parcelas[i] = a.parcelas[i]+acrecimo;
+    function alteraValorMesesRestantes(
+        uint _codigo,
+        uint256 startMes,
+        uint256 acrecimo
+    ) public view 
+    validaExistenciaContrato(_codigo)
+    validaMes(startMes)
+    validaValorParcela(acrecimo)
+    returns (bool) {
+        Aluguel memory a = alugueis[_codigo];
+        for (uint256 i = startMes; i < a.parcelas.length; i++) {
+            a.parcelas[i] = a.parcelas[i] + acrecimo;
         }
         return true;
     }
-
-
 }
